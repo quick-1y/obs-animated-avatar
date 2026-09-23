af6b3d083721067d9587375086945745cbafa1de
qu1ck1y <l0nglife3025@gmail.com>
2026-09-23 18:19:46 +0300

    fix(01): WR-01 add GPL license headers to plugin-main.cpp and avatar-source.cpp

--- src/plugin-main.cpp
+++ src/plugin-main.cpp
@@ -1,6 +1,6 @@
 /*
-Plugin Name
-Copyright (C) <Year> <Developer> <Email Address>
+Animated Avatar Plugin for OBS
+Copyright (C) 2026 quick-1y <l0nglife3025@gmail.com>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
